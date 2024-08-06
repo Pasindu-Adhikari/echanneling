a8b955468deceaf0ced093f34b67f22f534d5041
Pasindu-Adhikari <pasindumax39@gmail.com>
2024-08-06 21:06:37 +0530

    echanneling

---
+++ index.php
@@ -0,0 +1,137 @@
+</html>
+<!DOCTYPE html>
+<html lang = "en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0">
+
+        <title>E-channeling</title>
+
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+    <body>
+        <!--Navigation-->
+            <nav>  
+                    <img src="img/medical logo-3.jpg" alt=""></a>
+                <div class="navigation">
+            
+                <ul>
+                    <li><a class="active" href="#">Home</a></li>
+                    <li><a href="about.html">About</a></li>
+                    <!-- <li><a href="#">Contact</a></li> -->
+                    <li><a href="login.php">Find a Doctor</a></li>
+                    <li><a href="login.php">Login</a></li>
+                    
+                </ul>
+                </div>
+            </nav>
+
+            <section id="home">
+                <h1>WELLCOME to E-channeling Center<br>
+                    We Provide
+                    <br>  Total Health  Care Solution </h1>
+                
+                    <p></p>
+               
+                <div class="btn-home">            
+                <a href="login.php" class="yellow-btn">Find a Doctor</a>
+                </div>
+                <div class="btn-home">            
+                    <a href="login.php" class="yellow-btn">Book Appointment</a>
+                </div>
+                    
+            </section>
+
+                <!--Story-->
+                <section id="Story">
+                    <div class="Story-img"> 
+                        <img src="img/doctors-2.jpg" alt="">
+                    </div>
+                    <div class="Story-text">
+                        <h2>About Our Clinic</h2>
+                        <p></p>
+                        <div class="Story-number">
+                            <div class="Story-nymber-box bx-1">
+                                <strong>120+</strong>
+                                <span>Expert Doctor's</span>
+                            </div>
+                            <div class="Story-nymber-box bx-2">
+                                <strong>10000+</strong>
+                                <span>Happy Patient's</span>
+                            </div>
+                            <div class="Story-nymber-box bx-3">
+                                <strong>400+</strong>
+                                <span>Hospital Room's</span>
+                            </div>
+                            <div class="Story-nymber-box bx-4">
+                                <strong>80+</strong>
+                                <span>Award winning</span>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <!--Story end-->    
+
+
+    
+
+                <footer>
+                    <div class="footer-col">
+                        <h3>Quick Links</h3>
+                        <li>Phone: 0760022701</li>
+                        <li>Email:echannelin@gmail.com</li>
+                        <li>Address:303 Colombo Rd, Kurunegala, Sri Lanka</li>
+                        
+                    </div>
+                    <div class="footer-col">
+                        <h3>Quick Links</h3>
+                        <li>Home</li>
+                        <li>About Us</li>
+                        <li>Services</li>
+                        <li>Privacy Policy</li>
+                        <li>Terms and Conditions</li>
+                    </div>
+                    <div class="footer-col">
+                        <h3>Resources</h3>
+                        <li>Guiders</li>
+                        <li>Research</li>
+                        <li>Experts</li>
+                        <li>Marketting Service</li>
+                    </div>
+                    <div class="footer-col">
+                        <h3>Social Media</h3>
+                        <li>Facebook</li>
+                        <li>Linkedin</li>
+                        <li>Instagram</li>
+                        <li>Youtube</li>
+                        <li>Twitter</li>
+                    </div>
+                    <div class="footer-col">
+                        <h3>Newsletter</h3>
+                        <p>You can trust us we only send promo offers.</p><br>
+                        <div class="subscribe">
+                            <input type="text" placeholder="Your Email Address">
+                            <a href="#" class="yellow">SUBSCRIBE</a>
+                        </div>
+                    </div>
+                    <div class="copy">
+                        <p></p>
+                    <div class="copy-links">
+                        <i class="fa fa-facebook"></i>
+                        <i class="fa fa-linkedin"></i>
+                        <i class="fa fa-instagram"></i>
+                        <i class="fa fa-youtube"></i>
+                        <i class="fa fa-twitter"></i>
+                    </div>
+                    </div>
+                </footer>
+
+
+
+    </body>
+
+    
+</html>
